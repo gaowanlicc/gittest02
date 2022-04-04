2027a313fbfacc7cd23d72c53b91470d4a827204
gao <2234204065@qq.com>
2022-04-04 17:50:58 +0800

    third commit

--- src/main/java/gittest02.java
+++ src/main/java/gittest02.java
@@ -4,5 +4,6 @@ public class gittest02 {
     public static void main(String[] args) {
         System.out.println("你好呀");
         System.out.println("今天心情还可以");
+        System.out.println("她理我了");
     }
 }
